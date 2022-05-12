e6ec4ad144b2c6a1e2f7a8ff5918312841215e80
matheusfillipe <matheusfillipeag@gmail.com>
2022-05-12 04:19:17 +0000

    Basic bridge

--- server.go
+++ server.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"time"
@@ -22,6 +24,7 @@ type Config struct {
 	listen_port    string
 	irc_host       string
 	irc_port       string
+  irc_ssl        bool
 	ssl_cert       string
 	ssl_key        string
 	allowed_origin string
@@ -34,12 +37,13 @@ func makeConfig() Config {
 		log.Fatal("Error loading .env file")
 	}
 
-  allowed_origin := os.Getenv("ALLOWED_ORIGIN")
+	allowed_origin := os.Getenv("ALLOWED_ORIGIN")
 	var config = Config{
 		listen_host:    os.Getenv("LISTEN_HOST"),
 		listen_port:    os.Getenv("LISTEN_PORT"),
 		irc_host:       os.Getenv("IRC_HOST"),
 		irc_port:       os.Getenv("IRC_PORT"),
+    irc_ssl:        os.Getenv("IRC_USE_SSL") == "true",
 		ssl_cert:       os.Getenv("SSL_CERT"),
 		ssl_key:        os.Getenv("SSL_KEY"),
 		allowed_origin: allowed_origin,
@@ -52,47 +56,101 @@ func makeConfig() Config {
 			},
 		},
 	}
-  debug("Allowing origin: %q", allowed_origin)
-  return config
+	debug("Allowing origin: %q", allowed_origin)
+	return config
 }
 
+func irc_connect(config Config) net.Conn {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", config.irc_host+":"+config.irc_port)
+	if err != nil {
+		println("ResolveTCPAddr failed:", err.Error())
+		return nil
+	}
+	debug("Starting IRC server connection")
+
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		println("Dial failed:", err.Error())
+		return nil
+	}
+	return conn
+}
+
+func bridge(ws_conn *websocket.Conn, irc_conn net.Conn) {
+	// Read from irc, Write to websocket
+	close := func() {
+		ws_conn.Close()
+		irc_conn.Close()
+	}
+
+	go func() {
+		for {
+			reader := bufio.NewReader(irc_conn)
+			for {
+				irc_message, err := reader.ReadString('\n')
+				if err != nil {
+					println("Read from irc server failed:", err.Error())
+					close()
+					return
+				}
+				debug("Irc Server message: %s", string(irc_message))
+
+				if err = ws_conn.WriteMessage(websocket.TextMessage, []byte(irc_message)); err != nil {
+					log.Println("Error writing message: ", err, " to ", ws_conn.RemoteAddr(), ". Closing connection")
+					close()
+					return
+				}
+			}
+		}
+	}()
+
+	// Read from websocket, Write to irc
+	for {
+		msgType, ws_message, err := ws_conn.ReadMessage()
+		if msgType == websocket.BinaryMessage {
+			debug("Ignoring websocket binary message")
+			continue
+		}
+		if err != nil {
+			println("Read from websocket server failed:", err.Error())
+			close()
+			return
+		}
+		debug("Message from websocket client: %s", ws_message)
+
+		// if _, err = irc_conn.Write([]byte(ws_message)); err != nil {
+		// 	log.Println("Error writing : ", err, " to irc server. Closing connection")
+		// 	close()
+		// 	return
+		// }
+	}
+}
 
 func main() {
-  config := makeConfig()
+	config := makeConfig()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		origin := r.Header.Get("Origin")
-		debug("Client origin: %q", origin)
-    upgrader := config.upgrader
-		conn, err := upgrader.Upgrade(w, r, nil)
+		upgrader := config.upgrader
+		ws_conn, err := upgrader.Upgrade(w, r, nil)
+		defer ws_conn.Close()
 		log.Println("New client from ", r.RemoteAddr)
+		debug("Client origin: %q", r.Header.Get("Origin"))
 		if err != nil {
 			log.Println(err)
 			return
 		}
-
-		for {
-			// Read message from browser
-			msgType, msg, err := conn.ReadMessage()
-			if err != nil {
-				return
-			}
-
-			// Print the message to the console
-			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
-
-			// Write message back to browser
-			if err = conn.WriteMessage(msgType, msg); err != nil {
-				return
-			}
+    irc_conn := irc_connect(config)
+		if irc_conn == nil {
+			return
 		}
+		bridge(ws_conn, irc_conn)
 	})
 
-  if config.ssl_cert != "" && config.ssl_key != "" {
-    log.Printf("Starting server with https on %v:%v", config.listen_host, config.listen_port)
-    log.Fatal(http.ListenAndServeTLS(config.listen_host+":"+config.listen_port, config.ssl_cert, config.ssl_key, nil))
-  } else {
-    log.Printf("Starting server on %v:%v", config.listen_host, config.listen_port)
-    log.Fatal(http.ListenAndServe(config.listen_host+":"+config.listen_port, nil))
-  }
+	if config.ssl_cert != "" && config.ssl_key != "" {
+		log.Printf("Starting server with https on %v:%v", config.listen_host, config.listen_port)
+		log.Fatal(http.ListenAndServeTLS(config.listen_host+":"+config.listen_port, config.ssl_cert, config.ssl_key, nil))
+	} else {
+		log.Printf("Starting server on %v:%v", config.listen_host, config.listen_port)
+		log.Fatal(http.ListenAndServe(config.listen_host+":"+config.listen_port, nil))
+	}
 }
